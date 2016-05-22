ec2051cff05cabf32097480c82edb8cfb544f43d
code-4-fun <Devendra@Mac>
2016-05-23 00:23:34 +0900

    initial commit

---
+++ public/javascripts/geolocation.js
@@ -0,0 +1,19 @@
+angular.module('geoLocateModule', ['mapModule']);
+
+angular.module('geoLocateModule')
+  .controller('geoLocateCtrl', ['$scope', 'geolocation',
+      function ($scope, geolocation) {
+
+        $scope.current = {};
+
+        geolocation.getLocation()
+            .then( function ( data ) {
+                  var coords = {lat: data.coords.latitude, long: data.coords.longitude};
+                  // Set the latitude and longitude equal to the HTML5 coordinates
+                  $scope.current.longitude = parseFloat(coords.long).toFixed(3);
+                  $scope.current.latitude = parseFloat(coords.lat).toFixed(3);
+                  console.log('Current Location:', $scope.current);
+                }
+            );
+      }]
+  );
---
+++ public/javascripts/maps.js
@@ -0,0 +1,72 @@
+angular.module('mapModule', ['uiGmapgoogle-maps', 'geolocation']);
+
+angular.module('mapModule')
+  .config(function(uiGmapGoogleMapApiProvider) {
+      uiGmapGoogleMapApiProvider.configure({
+          key: 'AIzaSyB-M1Rex82qJyysYDqqovByt3VcR7e60bk',
+          libraries: 'geometry,visualization'
+      });
+  });
+
+angular.module('mapModule')
+  .controller('mapController', ['$scope', 'mapService', 'uiGmapGoogleMapApi', 'geolocation',
+      function ($scope, mapService, uiGmapGoogleMapApi, geolocation) {
+
+        $scope.map = {};
+        $scope.isMapReady = false;
+        $scope.defaultLocation = mapService.defaultLocation;
+        $scope.currentLocation = mapService.defaultLocation;
+        $scope.options = {scrollwheel: false};
+        $scope.markers = [];
+
+        uiGmapGoogleMapApi.then(function(maps) {
+            $scope.map = maps;
+            geolocation.getLocation().then( function ( data ) {
+                var coords = {lat: data.coords.latitude, long: data.coords.longitude};
+                var current = {};
+                $scope.currentLocation.center.longitude = parseFloat(coords.long).toFixed(3);
+                $scope.currentLocation.center.latitude = parseFloat(coords.lat).toFixed(3);
+                $scope.isMapReady = true;
+                $scope.markers.push(
+                  mapService.addMarker(
+                        $scope.markers.length,
+                        $scope.currentLocation.center.latitude,
+                        $scope.currentLocation.center.longitude,
+                        'Current Location'
+                  )
+                );
+            });
+        });
+      }]);
+
+angular.module('mapModule')
+  .factory('mapService', ['$rootScope', 'geolocation',
+      function ($rootScope, geolocation) {
+
+        var zoomLevel = 4;
+
+        var mapService = {};
+        mapService.defaultLocation = { center: { latitude: 45, longitude: -73 }, zoom: zoomLevel};
+
+        var Point = function (id, latitude, longitude, message) {
+            this.id = id;
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.message = message;
+            this.icon = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
+        };
+
+        mapService.addMarker = function (id, latitude, longitude, message) {
+            var point = new Point(
+                id,
+                latitude,
+                longitude,
+                message
+            );
+            console.log('marker prepared: ', point);
+            return point;
+        };
+
+        return mapService;
+
+      }]);
